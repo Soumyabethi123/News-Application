669e3878f1d7323cc4dcbf729c06d3cfa9597dbd
Soumyabethi123 <soumyab_t20012@students.isquareit.edu.in>
2022-12-14 12:17:06 +0530

    Added Custom tabs

--- app/src/main/java/com/example/newsfresh/adapter/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsfresh/adapter/NewsListAdapter.kt
@@ -1,18 +1,19 @@
 package com.example.newsfresh.adapter
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.newsfresh.News
 import com.example.newsfresh.R
 
 class NewsListAdapter(val context : Context , val  News : List<News>) : RecyclerView.Adapter<NewsListAdapter.NewsViewHolder>(){
-
 
     class NewsViewHolder(itemview : View) : RecyclerView.ViewHolder(itemview){
 
@@ -24,7 +25,9 @@ class NewsListAdapter(val context : Context , val  News : List<News>) : Recycler
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
+
         return NewsViewHolder(view)
+
     }
 
     override fun getItemCount(): Int {
@@ -40,8 +43,16 @@ class NewsListAdapter(val context : Context , val  News : List<News>) : Recycler
         holder.Desc.text=current.description
 
         Glide.with(context).load(current.urlToImage).into(holder.newsimage)
-    }
 
+        //var newsimage = itemview.findViewById<ImageView>(R.id.imageview4)
+
+        holder.itemView.setOnClickListener{
+            //onclick(current.url)
+            val builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(context,Uri.parse(current.url))
+        }
+    }
 }
 
 
